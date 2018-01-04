30106c5a819a6f09836006f593cc089b31f46335
Sanchez <JuanDavid.Sanchez@mjn.com>
2018-01-04 18:53:36 -0500

    Updated Style

--- ui.R
+++ ui.R
@@ -22,7 +22,7 @@ shinyUI(semanticPage(
   div(style ='min-height: 100%',
       div(
         div(class = 'ui four item stackable container menu',
-            div(class = 'item', img(src='../img/rb_logo_220.png')),
+            div(class = 'item', img(src='../img/rb_logo_220.png', style = 'width:220px')),
             a(class = 'item', id="forecast_actions", uiicon('hourglass end icon'), 'Forecast'),
             a(class = 'item', id="history_actions", uiicon('archive icon'), 'Historic data'),
             a(class = 'item', id="doi_actions", uiicon('cube icon'), 'Days of Inventory'),
