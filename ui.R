88ab854b454a1a3a284c0c03a0f056fcdf944d96
Sanchez <JuanDavid.Sanchez@mjn.com>
2018-01-04 18:14:55 -0500

    Added menu

--- ui.R
+++ ui.R
@@ -9,14 +9,24 @@
 
 library(shiny)
 library(shiny.semantic)
+library(shinyjs)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 shinyUI(semanticPage(
   
   # Application title
   title ="Prophet (rb-mjn)",
+  shinyjs::useShinyjs(),
   suppressDependencies('bootstrap'),
-  
-  # Sidebar with a slider input for number of bins 
-  div(class = 'ui button', uiicon('user'), "Icon button")
+  div(style ='min-height: 100%',
+      div(
+        div(class = 'ui four item stackable container menu',
+            div(class = 'item', img(src='img/rb_logo.png'), style = 'width:135px'),
+            a(class = 'item', id="forecast_actions", uiicon('hourglass end icon'), 'Forecast'),
+            a(class = 'item', id="history_actions", uiicon('archive icon'), 'Historic data'),
+            a(class = 'item', id="doi_actions", uiicon('cube icon'), 'Days of Inventory'),
+            a(class = 'item', id="logout_action", 'Logout'))
+        )
+      )
 ))
