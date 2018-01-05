c0a594c5668b9b5568bcd18d5fd22a2049050ebf
Sanchez <JuanDavid.Sanchez@mjn.com>
2018-01-05 10:06:19 -0500

    Changed index (route failed!)

--- helpers.R
+++ helpers.R
@@ -9,7 +9,7 @@ library(shiny.router)
 headerMenu <- (
   div(class = 'ui five item stackable container menu',
       div(class = 'item', 
-          a(href="/", img(src='../img/rb_mjn_logo.png', style = 'width: 95px;'))
+          a(href="/index", img(src='../img/rb_mjn_logo.png', style = 'width: 95px;'))
           ),
       a(class = 'item', href="/forecast", uiicon('hourglass end icon'), 'Forecast'),
       a(class = 'item', href="/history", uiicon('archive icon'), 'Historic data'),
@@ -34,6 +34,6 @@ root_page <- renderedPage("Home", "LoremIpsum 11")
 other_page <- renderedPage("Other", "Loremipsum 2")
 
 router <- make_router(
-  route("/", root_page),
+  route("/index", root_page),
   route("/forecast", other_page)
 )
