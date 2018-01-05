ce34d9b06a1a17eb8456f71329d1b5f72f81b12c
Sanchez <JuanDavid.Sanchez@mjn.com>
2018-01-04 19:17:27 -0500

    Fixed wrapper and logout icon

--- ui.R
+++ ui.R
@@ -20,12 +20,12 @@ shinyUI(semanticPage(
   shinyjs::useShinyjs(),
   suppressDependencies('bootstrap'),
   div(class = 'wrapper', style ='min-height: 100%',
-      div(class = 'ui four item stackable container menu',
-          #div(class = 'item', img(src='../img/rb_logo_220.png', style = 'height: 30px;')),
+      div(class = 'ui five item stackable container menu',
+          div(class = 'item', img(src='../img/rb_logo_220.png', style = 'height: 30px;')),
           a(class = 'item', id="forecast_actions", uiicon('hourglass end icon'), 'Forecast'),
           a(class = 'item', id="history_actions", uiicon('archive icon'), 'Historic data'),
           a(class = 'item', id="doi_actions", uiicon('cube icon'), 'Days of Inventory'),
-          a(class = 'item', id="logout_action", 'Logout')
+          a(class = 'item', id="logout_action", uiicon('logout icon'), 'Logout')
           )
       )
 ))
